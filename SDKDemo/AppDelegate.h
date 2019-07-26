11d68c058c69dd63bd71eb08babc5e41b15de897
Lester Ji <xiaoji520524@163.com>
2019-07-26 14:22:30 +0800

    Initial Commit

---
+++ SDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDKDemo
+//
+//  Created by cy on 2019/7/26.
+//  Copyright © 2019 Lester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDKDemo
+//
+//  Created by cy on 2019/7/26.
+//  Copyright © 2019 Lester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
